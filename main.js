cae61ad3b43f49921f461f8441ee6c779151a091
MarcoLotti <gorlandelli64@gmail.com>
2022-01-11 17:08:01 +0100

    prima card inserita con js

--- main.js
+++ main.js
@@ -30,3 +30,18 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+/* prima card */
+document.querySelector('.team-container').innerHTML = `
+<div class="team-card">
+    <div class="card-image">
+      <img
+          src="img/wayne-barnett-founder-ceo.jpg"
+          alt="Wayne Barnett"
+        />
+    </div>
+    <div class="card-text">
+        <h3>Wayne Barnett</h3>
+        <p>Founder & CEO</p>
+    </div>
+</div>`;
